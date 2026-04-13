c45fb25e9d131335a82a2286cbfe6006103dbfb4
D-Shatnev <dmitry.shatnev@mail.ru>
2026-04-13 16:56:23 +0300

    feat(pipeline): Add package init docstring for pipeline module

---
+++ src/dataton_tri_losya_49/pipeline/__init__.py
@@ -0,0 +1,6 @@
+"""
+Modular speaker recognition pipeline.
+
+This package contains building blocks (encoder, audio loader, indexer, evaluator)
+and a runner that orchestrates inference -> indexing -> evaluation.
+"""
