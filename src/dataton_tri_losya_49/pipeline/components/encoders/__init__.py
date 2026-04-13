b2e96ea9a3957b59e925804ed0c5644251c7cebd
D-Shatnev <dmitry.shatnev@mail.ru>
2026-04-13 16:00:47 +0300

    feat(encoders): Add package init exporting OnnxEncoder
    Create encoders package initializer with module docs and __all__
    to expose OnnxEncoder as the public encoder implementation.

---
+++ src/dataton_tri_losya_49/pipeline/components/encoders/__init__.py
@@ -0,0 +1,12 @@
+"""
+Encoder components.
+
+This package provides encoder implementations compatible with the
+dataton_tri_losya_49.pipeline.interfaces.Encoder protocol.
+
+Encoders map a batch of waveforms with shape (B, T) to embeddings with shape (B, D).
+"""
+
+from dataton_tri_losya_49.pipeline.components.encoders.onnx_encoder import OnnxEncoder
+
+__all__ = ["OnnxEncoder"]
